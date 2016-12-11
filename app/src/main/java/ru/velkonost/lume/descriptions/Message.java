f9e7bc9d05b75b0b97eed2c60e8554b8754ff202
Artem <klimenkocpp@icloud.com>
2016-12-11 13:52:22 +0600

    [7] - created Message description.

--- app/src/main/java/ru/velkonost/lume/descriptions/Message.java
+++ app/src/main/java/ru/velkonost/lume/descriptions/Message.java
@@ -1,4 +1,84 @@
 package ru.velkonost.lume.descriptions;
 
-public class Message {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Message implements Parcelable {
+
+    private int id;
+    private int userId;
+    private int dialogId;
+    private int mStatus;
+    private String mText;
+    private String mDate;
+
+    public Message(int id, int userId, int dialogId, int status, String text, String date) {
+        this.id = id;
+        this.userId = userId;
+        this.dialogId = dialogId;
+        mStatus = status;
+        mText = text;
+        mDate = date;
+    }
+
+    private Message(Parcel in) {
+        id = in.readInt();
+        userId = in.readInt();
+        dialogId = in.readInt();
+        mStatus = in.readInt();
+        mText = in.readString();
+        mDate = in.readString();
+
+    }
+
+    public static final Creator<Message> CREATOR = new Creator<Message>() {
+        @Override
+        public Message createFromParcel(Parcel in) {
+            return new Message(in);
+        }
+
+        @Override
+        public Message[] newArray(int size) {
+            return new Message[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeInt(userId);
+        parcel.writeInt(dialogId);
+        parcel.writeInt(mStatus);
+        parcel.writeString(mText);
+        parcel.writeString(mDate);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getDialogId() {
+        return dialogId;
+    }
+
+    public int getStatus() {
+        return mStatus;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
 }
