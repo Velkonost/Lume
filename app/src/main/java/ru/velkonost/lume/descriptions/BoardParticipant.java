7341c2f620907caa04e486031b696af87a233e89
Artem <klimenkocpp@icloud.com>
2017-01-11 23:25:30 +0600

    [9] - board welcome: added board participant description.

--- app/src/main/java/ru/velkonost/lume/descriptions/BoardParticipant.java
+++ app/src/main/java/ru/velkonost/lume/descriptions/BoardParticipant.java
@@ -1,8 +1,54 @@
 package ru.velkonost.lume.descriptions;
 
-/**
- * Created by KlimenkoCpp on 11.01.17.
- */
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class BoardParticipant {
+public class BoardParticipant implements Parcelable {
+
+    private int id;
+    private int avatar;
+    private String login;
+    private boolean last;
+    private int reminded;
+
+    public BoardParticipant(int id, int avatar, String login, boolean last, int reminded) {
+        this.id = id;
+        this.avatar = avatar;
+        this.login = login;
+        this.last = last;
+        this.reminded = reminded;
+    }
+
+    private BoardParticipant(Parcel in) {
+        id = in.readInt();
+        avatar = in.readInt();
+        login = in.readString();
+        reminded = in.readInt();
+    }
+
+    public static final Creator<BoardParticipant> CREATOR = new Creator<BoardParticipant>() {
+        @Override
+        public BoardParticipant createFromParcel(Parcel in) {
+            return new BoardParticipant(in);
+        }
+
+        @Override
+        public BoardParticipant[] newArray(int size) {
+            return new BoardParticipant[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeInt(avatar);
+        parcel.writeString(login);
+        parcel.writeInt(reminded);
+    }
+
 }
