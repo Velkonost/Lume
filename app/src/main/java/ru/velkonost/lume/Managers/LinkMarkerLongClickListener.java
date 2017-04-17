8bb7f9d6fa7b68c07fe0b2ded21f54a87be8c441
Artem <klimenkocpp@icloud.com>
2017-04-17 22:30:07 +0600

    some changes

--- app/src/main/java/ru/velkonost/lume/Managers/LinkMarkerLongClickListener.java
+++ app/src/main/java/ru/velkonost/lume/Managers/LinkMarkerLongClickListener.java
@@ -7,15 +7,34 @@ import com.google.android.gms.maps.model.Marker;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Слушатель для обработки долгого нажатия на маркер карты
+ */
 public abstract class LinkMarkerLongClickListener implements GoogleMap.OnMarkerDragListener {
 
+    /**
+     * Свойство - предыдущая позиция
+     */
     private int previousIndex = -1;
 
+    /**
+     * Свойство - выбранный маркер
+     */
     private Marker cachedMarker = null;
+
+    /**
+     * Свойство - позиция выбранного маркера
+     */
     private LatLng cachedDefaultPostion = null;
 
+    /**
+     * Свойство - список маркеров
+     */
     private List<Marker> markerList;
+
+    /**
+     * Свойство - список позиций маркеров
+     */
     private List<LatLng> defaultPostions;
 
     public LinkMarkerLongClickListener(List<Marker> markerList){
